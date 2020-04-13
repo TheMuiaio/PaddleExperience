c96d6caad57f1d5bb4e9d75cf3c9d1edf7d6b5cc
victo <victo@DESKTOP-TC551KU>
2020-04-13 21:00:43 +0200

    Lechuga

--- src/paddleexperience/PaddleExperience.java
+++ src/paddleexperience/PaddleExperience.java
@@ -22,7 +22,7 @@ public class PaddleExperience extends Application {
         Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
         
         Scene scene = new Scene(root);
-        
+        //Lechuga
         stage.setScene(scene);
         stage.show();
     }
