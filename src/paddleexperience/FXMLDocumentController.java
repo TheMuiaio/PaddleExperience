929864478d96c2f07c6f8d4af6781789f90c2a54
Gerard <Gerard@DESKTOP-O1U5MR0>
2020-04-13 20:49:53 +0200

    No res

---
+++ src/paddleexperience/FXMLDocumentController.java
@@ -0,0 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package paddleexperience;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableView;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+
+import static DBAcess.ClubDBAccess.getSingletonClubDBAccess;
+import java.io.IOException;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+
+        
+/**
+ *
+ * @author Gerard
+ */
+public class FXMLDocumentController implements Initializable {
+    
+    //hola
+    
+    @FXML
+    private ImageView backDate;
+    @FXML
+    private Label dateLabel;
+    @FXML
+    private ImageView forwardDate;
+    @FXML
+    private TableView<?> pista1;
+    @FXML
+    private TableView<?> pista2;
+    @FXML
+    private TableView<?> pista3;
+    @FXML
+    private TableView<?> pista4;
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+    @FXML
+    private void toLogIn(ActionEvent event) throws IOException {
+          ((Node) event.getSource()).getScene().setRoot(FXMLLoader.load(getClass().getResource("FXMLLogin.fxml")));
+    }
+
+    @FXML
+    private void previousDay(MouseEvent event) {
+    }
+
+    @FXML
+    private void nextDay(MouseEvent event) {
+    }
+    
+}
