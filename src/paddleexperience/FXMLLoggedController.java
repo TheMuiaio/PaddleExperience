75d77ae18d027f880c7ac3c6eff733c52dcc7153
Germol <47815848+TheMuiaio@users.noreply.github.com>
2020-04-18 01:47:44 +0200

    null court issue found

--- src/paddleexperience/FXMLLoggedController.java
+++ src/paddleexperience/FXMLLoggedController.java
@@ -14,24 +14,6 @@ import javafx.scene.control.TableView;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 
-import DBAcess.ClubDBAccess;
-import java.io.IOException;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.LocalTime;
-import java.time.format.DateTimeFormatter;
-import java.time.format.FormatStyle;
-import javafx.event.ActionEvent;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Cursor;
-import javafx.scene.Node;
-import javafx.scene.control.DateCell;
-import javafx.scene.control.DatePicker;
-import javafx.scene.layout.GridPane;
-import model.Booking;
-import model.Member;
-import paddleexperience.CurrentUser;
-
 /**
  * FXML Controller class
  *
@@ -45,109 +27,29 @@ public class FXMLLoggedController implements Initializable {
     private Label dateLabel;
     @FXML
     private ImageView forwardDate;
-    
-    private LocalDate dia;
+    @FXML
+    private TableView<?> pista1;
+    @FXML
+    private TableView<?> pista2;
+    @FXML
+    private TableView<?> pista3;
+    @FXML
+    private TableView<?> pista4;
 
-    private Member member;
-    
-    private ClubDBAccess clubDBAccess;
-    @FXML
-    private DatePicker datePicker;
-    @FXML
-    private GridPane taula;
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-        dia = LocalDate.now();
-        clubDBAccess = ClubDBAccess.getSingletonClubDBAccess();
-        member = CurrentUser.getMembre();
-        changeDateLabel();
-        datePicker.getEditor().setEditable(false);
-        datePicker.getEditor().setVisible(false);
-        
-        datePicker.setDayCellFactory((DatePicker picker) -> {
-            return new DateCell() {
-                @Override
-                public void updateItem(LocalDate date, boolean empty) {
-                    super.updateItem(date, empty);
-                    LocalDate today = LocalDate.now();
-                    setDisable(empty || date.compareTo(today) < 0);
-                }
-            };
-        });
     }    
 
     @FXML
-    private void previousDay(ActionEvent event) {
-        LocalDate aux = dia.minusDays(1);
-        if (aux.compareTo(LocalDate.now()) >= 0) {
-            dia = aux;
-            changeDateLabel();
-        }
+    private void previousDay(MouseEvent event) {
     }
 
     @FXML
-    private void nextDay(ActionEvent event) {
-        dia = dia.plusDays(1);
-        changeDateLabel();
-    }
-
-    @FXML
-    private void onProfile(ActionEvent event) throws IOException {
-        ((Node) event.getSource()).getScene().setRoot(FXMLLoader.load(getClass().getResource("FXMLUserInfo.fxml")));
-    }
-
-    @FXML
-    private void newBooking(ActionEvent event) {
-        DatePicker dp = new DatePicker();
-        dp.setDayCellFactory((DatePicker picker) -> {
-            return new DateCell() {
-                @Override
-                public void updateItem(LocalDate date, boolean empty) {
-                    super.updateItem(date, empty);
-                    LocalDate today = LocalDate.now();
-                    setDisable(empty || date.compareTo(today) < 0);
-                }
-            };
-        });
-        clubDBAccess.getBookings().add(new Booking(LocalDateTime.now(), dp.getValue(), LocalTime.now(), member.getCreditCard() != null, clubDBAccess.getCourt("Court 1"), member));
-        //clubDBAccess.getBookings().add()
-        
-    }
-
-    @FXML
-    private void toMyBookings(ActionEvent event) throws IOException {
-        ((Node) event.getSource()).getScene().setRoot(FXMLLoader.load(getClass().getResource("FXMLReserves.fxml")));
-    }
-    
-    private void changeDateLabel() {
-        dateLabel.setText(dia.format(DateTimeFormatter.ofLocalizedDate(FormatStyle.FULL)));
-    }
-
-    @FXML
-    private void outLink(MouseEvent event) {
-        try {
-            dateLabel.getScene().setCursor(Cursor.DEFAULT);
-        }
-        catch(NullPointerException e){}
-    }
-
-    @FXML
-    private void onLink(MouseEvent event) {
-        try {
-            dateLabel.getScene().setCursor(Cursor.HAND);
-        }
-        catch(NullPointerException e){}
-    }
-
-    @FXML
-    private void pickDate(ActionEvent event) {
-        dia = datePicker.getValue();
-        datePicker.hide();
-        changeDateLabel();
+    private void nextDay(MouseEvent event) {
     }
     
 }
